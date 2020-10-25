e64eec556d373b5301ea221122cac471d5f7cf39
Bartek Dec <decbartek@gmail.com>
2020-10-25 19:19:36 +0100

    created ShopSTART class

---
+++ src/main/java/com/example/shop/ShopSTART.java
@@ -0,0 +1,26 @@
+package com.example.shop;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("start")
+public class ShopSTART extends Shop {
+
+    @Autowired
+    public ShopSTART(ShopStorage shopStorage, Cart cart, Util util) {
+        super(shopStorage, cart, util);
+    }
+
+    public void displayCart() {
+        System.out.println("Your products:");
+        util.displayProducts(cart.getUserProducts());
+
+        double totalValue = cart.getUserProducts().stream()
+                .map(Product::getPrice)
+                .reduce(0.0, Double::sum);
+
+        System.out.printf("Total value of products is: %.2f\n\n", totalValue);
+    }
+}
