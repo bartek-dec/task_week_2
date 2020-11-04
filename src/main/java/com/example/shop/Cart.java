dd87048a38a26f615aba86f49a4d25786baa08cf
Bartek Dec <decbartek@gmail.com>
2020-11-04 12:30:02 +0100

    added addProduct() method and getTotalValue() method

--- src/main/java/com/example/shop/Cart.java
+++ src/main/java/com/example/shop/Cart.java
@@ -13,5 +13,15 @@ public class Cart {
     public List<Product> getUserProducts() {
         return userProducts;
     }
+
+    public void addProduct(Product product) {
+        userProducts.add(product);
+    }
+
+    public double getTotalValue() {
+        return userProducts.stream()
+                .map(Product::getPrice)
+                .reduce(0.0, Double::sum);
+    }
 }
 
